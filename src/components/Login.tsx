bfc44a97c5c56aca46bd226496425f19a3a04c5f
Christopher ONeill <chris@stytch.com>
2025-12-03 14:46:12 -0800

    Custom domain fix

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -40,7 +40,7 @@ const Login = ({ returnTo = null }: LoginProps) => {
       ...discoveryConfig.oauthOptions,
       discoveryRedirectURL: buildDiscoveryRedirectURL(),
     },
-    dfppaUrl: 'auth.notelydemo.com',
+    dfppaDomain: 'auth.notelydemo.com',
   };
 
   return (
